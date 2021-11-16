bf611bb65f32af11d3a031108a147a1298b08eba
Aoyon123 <49103044+Aoyon123@users.noreply.github.com>
2021-11-16 11:06:43 +0600

    The Final Final Project

--- script.js
+++ script.js
@@ -28,6 +28,25 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".highscore").textContent = highscore;
     }
 
+    //when guess is wrong
+    // shortcut of too high and too low
+    /*
+  }
+  else if(guess !== secretNumber){
+    if(score>1){
+      document.querySelector('.message').textContent=guess>secretNumber ? 'Too high!'
+:'Too Low';
+    score--;
+    document.querySelector('.score').textContent=score;
+    }
+    else{
+      document.querySelector('.message').textContent='You Lost The Game!';
+      document.querySelector('.score').textContent=0;
+    }
+}
+}
+*/
+
     //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
